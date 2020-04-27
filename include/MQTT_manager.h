238bfb1753462b581a04f7046f768c7092a9f36a
Lorenzo Decaria <lorenzo.decaria@gmail.com>
2020-04-27 15:07:01 -0700

    small refactor

--- include/MQTT_manager.h
+++ include/MQTT_manager.h
@@ -1,15 +1,9 @@
+#include "mqtt_client.h"
 
-typedef enum {
-    MQTT_CONNECTED,
-    MQTT_DISCONNECTED,
-    MQTT_SUBSCRIBED,
-    MQTT_UNSUBSCRIBED,
-    MQTT_PUBLISHED,
-    MQTT_RECEIVED_DATA,
-    MQTT_ERROR,
-} mqtt_connection_status_t;
 
-typedef void (*mqtt_callback_t)(mqtt_connection_status_t status);
+typedef void (*mqtt_callback_t)(esp_mqtt_event_handle_t event);
 
 void mqtt_init(const char * mqtt_broker, mqtt_callback_t);
 void mqtt_subscribe(const char * topic);
+void mqtt_publish(const char * topic, const char * message);
+void mqtt_reconnect();
--- src/MQTT_manager.c
+++ src/MQTT_manager.c
@@ -7,10 +7,11 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "MQTT_manager.h"
-#include "mqtt_client.h"
 #include "cJSON.h"
 #include "cJSON_Utils.h"
 #include "mdns.h"
+#include "freertos/queue.h"
+#include "mqtt_client.h"
 
 
 
@@ -28,206 +29,30 @@
 
 
 
-static EventGroupHandle_t mqtt_event_group;
 static const char TAG[] = "MQTT_Manager";
 static mqtt_callback_t mqtt_callback;
-static esp_mqtt_client_handle_t MQTTClient;
+static esp_mqtt_client_handle_t mqtt_client;
 
 
-void publish_status_task(void *pvParameter) {
-    // ESP_LOGI(TAG, "Starting publish_status Task");
 
-    // while (1) {
-    //     EventBits_t event_bits = xEventGroupWaitBits(event_group, THERMOSTAT_STATUS_UPDATED, pdTRUE, pdTRUE, PUBLISH_STATUS_TASK_BLOCK_TICKS);
-
-    //     if ( (event_bits & THERMOSTAT_STATUS_UPDATED) == THERMOSTAT_STATUS_UPDATED) {
-    //         // New data available
-    //         cJSON *root;
-    //         root = cJSON_CreateObject();
-            
-    //         float currentTemperature;
-    //         float targetTemperature;
-    //         CurrentHeatingCoolingState currentHeatingCoolingState;
-
-    //         if (thermostat.getCurrentHeatingCoolingState(&currentHeatingCoolingState) == ESP_OK) {
-    //             cJSON_AddNumberToObject(root, CurrentHeatingCoolingStateKey, currentHeatingCoolingState);
-    //         }
-
-    //         if (thermostat.getCurrentTemperature(&currentTemperature) == ESP_OK) {
-    //             cJSON_AddNumberToObject(root, CurrentTemperatureKey, currentTemperature);
-    //         }
-
-    //         if (thermostat.getTargetTemperature(&targetTemperature) == ESP_OK) {
-    //             cJSON_AddNumberToObject(root, TargetTemperatureKey, targetTemperature);
-    //         }
-
-    //         char *renderedJSON = cJSON_Print(root);
-
-    //         // Checking if MQTT client is connected
-    //         EventBits_t mqtt_event_bits = xEventGroupWaitBits(event_group, MQTT_CONNECTED_BIT, pdFALSE, pdTRUE, 0);
-
-    //         if ( (mqtt_event_bits & MQTT_CONNECTED_BIT) == MQTT_CONNECTED_BIT) {
-    //             // MQTT client connected, can publish
-    //             int msg_id = esp_mqtt_client_publish(MQTTClient, MQTT_THERMOSTAT_TOPIC, renderedJSON, 0, 1, 0);
-    //             xEventGroupClearBits(event_group, THERMOSTAT_STATUS_UPDATED);
-    //         } else {
-    //             // MQTT not connected, skipping publish
-    //             ESP_LOGI(TAG, "MQTT not connected, skipping");
-    //         }   
-    //     } else {
-    //         // xEventGroupWaitBits returned because of timeout
-    //         // TODO: handle missing DHT reading error
-    //         ESP_LOGI(TAG, "No thermostat update available");
-    //         esp_restart();
-    //     }
-    // }
-}
-
-void handleMQTTMessage( char* topic, const char* data) {
-    // cJSON *message = cJSON_Parse(data);
-    // if (message == NULL) {
-    //     const char *error_ptr = cJSON_GetErrorPtr();
-    //     if (error_ptr != NULL) {
-    //         ESP_LOGI(TAG, "Error parsing JSON: %s\n", error_ptr);
-    //     }
-    //     return;
-    // }
-
-    // if (cJSON_HasObjectItem(message, TargetHeatingCoolingStateKey)) {
-    //     cJSON *requestedState_JSON = cJSON_GetObjectItem(message, TargetHeatingCoolingStateKey);
-    //     if (cJSON_IsNumber(requestedState_JSON)) {
-    //         TargetHeatingCoolingState requestedState = (TargetHeatingCoolingState)requestedState_JSON->valueint;
-    //         thermostat.setTargetHeatingCoolingState(requestedState);
-    //     }
-    // }
-
-    // if (cJSON_HasObjectItem(message, TargetTemperatureKey)) {
-    //     cJSON *requestedTargetTemperature_JSON = cJSON_GetObjectItem(message, TargetTemperatureKey);
-    //     if (cJSON_IsNumber(requestedTargetTemperature_JSON)) {
-    //         float requestedTargetTemperature = requestedTargetTemperature_JSON->valuedouble;
-    //         thermostat.setTargetTemperature(requestedTargetTemperature);
-    //     }
-    // }
-
-    // cJSON_Delete(message);
-}
-
-static void mqtt_manager_task(void *args) {
-
-    EventBits_t bits;
-
-    while (1) {
-        bits = xEventGroupWaitBits(mqtt_event_group, MQTT_CONNECTED_BIT | MQTT_DISCONNECTED_BIT | MQTT_SUBSCRIBED_BIT | MQTT_UNSUBSCRIBED_BIT | MQTT_PUBLISHED_BIT | MQTT_RECEIVED_DATA_BIT | MQTT_ERROR_BIT, pdFALSE, pdFALSE, portMAX_DELAY); 
-        if (bits & MQTT_CONNECTED_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_CONNECTED_BIT);
-            mqtt_callback(MQTT_CONNECTED);
-        } else if (bits & MQTT_DISCONNECTED_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_DISCONNECTED_BIT);
-            mqtt_callback(MQTT_DISCONNECTED);
-        } else if (bits & MQTT_SUBSCRIBED_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_SUBSCRIBED_BIT);
-            mqtt_callback(MQTT_SUBSCRIBED);
-        } else if (bits & MQTT_UNSUBSCRIBED_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_UNSUBSCRIBED_BIT);
-            mqtt_callback(MQTT_UNSUBSCRIBED);
-        } else if (bits & MQTT_PUBLISHED_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_PUBLISHED_BIT);
-            mqtt_callback(MQTT_UNSUBSCRIBED);
-        } else if (bits & MQTT_RECEIVED_DATA_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_RECEIVED_DATA_BIT);
-            mqtt_callback(MQTT_PUBLISHED);
-        } else if (bits & MQTT_ERROR_BIT) {
-            xEventGroupClearBits(mqtt_event_group, MQTT_ERROR_BIT);
-            mqtt_callback(MQTT_ERROR);
-        } else {
-            ESP_LOGE(TAG, "UNEXPECTED EVENT");
-        }
-    }
-
-    vEventGroupDelete(mqtt_event_group);
-}
-
-// MQTT
-static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event){
-    BaseType_t xHigherPriorityTaskWoken, xResult;
-    xHigherPriorityTaskWoken = false;
-    xResult = ESP_OK;
-
-    // your_context_t *context = event->context;
-    switch (event->event_id) {
-        case MQTT_EVENT_CONNECTED:
-            ESP_LOGI(TAG, "MQTT Client connected");
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_CONNECTED_BIT, &xHigherPriorityTaskWoken);
-            break;
-        case MQTT_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "MQTT Client disconnected");
-            ESP_LOGI(TAG, "Reconnecting MQTT client");
-            esp_mqtt_client_start(MQTTClient);
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_DISCONNECTED_BIT, xHigherPriorityTaskWoken);
-            break;
-        case MQTT_EVENT_SUBSCRIBED:
-            // ESP_LOGI(TAG, "MQTT client subscribed to %s", event->topic);
-            ESP_LOGI(TAG, "MQTT client subscribed to ");
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_SUBSCRIBED_BIT, xHigherPriorityTaskWoken);
-            break;
-        case MQTT_EVENT_UNSUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT client unsubscribed from %s", event->topic);
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_UNSUBSCRIBED_BIT, xHigherPriorityTaskWoken);
-            break;
-        case MQTT_EVENT_PUBLISHED:
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_PUBLISHED_BIT, xHigherPriorityTaskWoken);
-            break;
-        case MQTT_EVENT_DATA:
-            ESP_LOGI(TAG, "MQTT client received new message:\nTopic: %s\nData: %s", event->topic, event->data);
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_RECEIVED_DATA_BIT, xHigherPriorityTaskWoken);
-            handleMQTTMessage(event->topic, event->data);
-            break;
-        case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT client error!");
-            xResult = xEventGroupSetBitsFromISR(mqtt_event_group, MQTT_ERROR_BIT, xHigherPriorityTaskWoken);
-            break;
-        default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-            break;
-    }
-
-    // if( xResult != pdFAIL ) {
-    //     portYIELD_FROM_ISR();
-    // }
-
-    return ESP_OK;
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+    mqtt_callback(event);
 }
 
 void mqtt_client_init(char* hostname) {
-    esp_mqtt_client_config_t mqtt_cfg = {
-    .event_handle = &mqtt_event_handler,
-    .host = hostname,
-    .uri =  nullptr, 
-    .port = MQTT_PORT,               
-    .client_id = nullptr,               
-    .username = nullptr, 
-    .password = nullptr, 
-    .lwt_topic = nullptr, 
-    .lwt_msg = nullptr,
-    .lwt_qos = 0,                      
-    .lwt_retain = 0,
-    .lwt_msg_len = 0,
-    .disable_clean_session = true,
-    .keepalive = 120,
-    .disable_auto_reconnect = false,
-    .user_context = nullptr,
-    .task_prio = 5,
-    .task_stack = 6144,
-    .buffer_size = 1024,
-    .cert_pem = nullptr,
-    .client_cert_pem = nullptr,
-    .client_key_pem = nullptr,
-    .transport = MQTT_TRANSPORT_OVER_TCP,
-    .refresh_connection_after_ms = 0
-};
+    esp_mqtt_client_config_t mqtt_cfg = { 
+        .host = hostname
+    };
 
-    MQTTClient = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_start(MQTTClient);
+    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(mqtt_client, ESP_EVENT_ANY_ID, mqtt_event_handler, mqtt_client);
+    esp_mqtt_client_start(mqtt_client);
+}
+
+void mqtt_reconnect() 
+{
+    esp_mqtt_client_start(mqtt_client);
 }
 
 // mDNS
@@ -270,18 +95,20 @@ esp_err_t resolve_mdns_host(const char * host_name, char * resolvedIP)
 
 void mqtt_init(const char * mqtt_broker, mqtt_callback_t callback)
 {
-    mqtt_event_group = xEventGroupCreate();
-    
     mqtt_callback = callback;
     start_mdns_service();
     char hostname[15];
     if (resolve_mdns_host(mqtt_broker, hostname) == ESP_OK) {
         mqtt_client_init(hostname);
-        xTaskCreate(mqtt_manager_task, "MQTT Manager Task", 4096, NULL, 3, NULL);
     }
 }
 
 void mqtt_subscribe(const char * topic)
 {
-    esp_mqtt_client_subscribe(MQTTClient, topic, 0);
+    esp_mqtt_client_subscribe(mqtt_client, topic, 0);
+}
+
+void mqtt_publish(const char * topic, const char * message) 
+{
+    int msg_id = esp_mqtt_client_publish(mqtt_client, topic, message, 0, 1, 0);
 }
